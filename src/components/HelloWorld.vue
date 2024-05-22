ce628d40eb259da36cd4a0903be1e5c4be0593bc
anoukvanbogaert <vb.anouk@gmail.com>
2024-05-22 11:52:48 -0700

    attribute binding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-    </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-bind:id="myId">Hello my div: {{ myId }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'HelloWorld',
-    props: {
-        msg: String,
-    },
+  name: "HelloWorld",
+  data() {
+    return {
+      myId: "divId",
+    };
+  },
+  props: {
+    msg: String,
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+#divId {
+  background-color: red;
+}
+</style>
