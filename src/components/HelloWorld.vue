4dd0a196e562b6f0b2df89b7ebcee82dcbe0266a
anoukvanbogaert <vb.anouk@gmail.com>
2024-05-22 12:46:15 -0700

    event modifiers

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,38 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div v-bind:id="myId">Hello my div: {{ myId }}</div>
+    <button v-on:click="handleClick">Click</button>
+    <h1 id="header">Header</h1>
+
+    <form @submit.prevent="handleSubmit">
+      <input type="text" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <button @click.once="OnBtnClick">Increase</button>
+    <p>{{ number }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
+  methods: {
+    handleClick() {
+      const header = document.getElementById('header');
+      header.style.color = 'red';
+    },
+    handleSubmit() {
+      console.log('form submitted');
+    },
+    OnBtnClick() {
+      this.number = this.number + 1;
+    },
+  },
   data() {
     return {
-      myId: "divId",
+      myId: 'divId',
+      number: 1,
     };
   },
   props: {
